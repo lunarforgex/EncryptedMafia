58d4268dbe7adda6b5189b35c104bcc17b8f8b44
lunarforgex <HahlKaiama12051989mcv@hotmail.com>
2025-10-18 23:19:18 +0800

    deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,26 @@
+import * as dotenv from "dotenv";
+
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+dotenv.config();
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, log } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  if (!process.env.INFURA_API_KEY) {
+    log("Warning: INFURA_API_KEY is not defined. Remote deployments may fail.");
+  }
+
+  const encryptedMafia = await deploy("EncryptedMafia", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  log(`EncryptedMafia contract deployed at ${encryptedMafia.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_mafia";
+func.tags = ["EncryptedMafia"];
